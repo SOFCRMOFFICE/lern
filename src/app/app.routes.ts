dd2bed3b6ee77874fb3448ba0e96a7340bce1725
SOFCRMOFFICE <biggest98763@gmail.com>
2026-01-09 16:56:05 +0530

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeModernComponent } from './pages/home-modern/home-modern';
-
-export const routes: Routes = [
-  { path: '', component: HomeModernComponent },
-  { path: '**', redirectTo: '' },
-];
